54c8c47836739ac19c662e4cfe0d7a0e12c3cf0e
Jordan Handy <handy.jordan@gmail.com>
2021-07-03 19:59:23 -0400

    add JS as next element on tree

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,4 +2,4 @@ var aboutParagraph = document.createElement("p");
 var textNode = document.createTextNode("My name is Jordan Handy. Feel free to explore my website, and view some of the work that I've done.");
 aboutParagraph.appendChild(textNode);
 var element = document.querySelector("h2");
-element.appendChild(aboutParagraph);
+element.append(aboutParagraph);
