6c50bd21e7e9198eef10353234686ca8a60e5343
Jordan Handy <handy.jordan@gmail.com>
2021-07-03 20:01:55 -0400

    add element as adjacent

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,5 +1,2 @@
-var aboutParagraph = document.createElement("p");
-var textNode = document.createTextNode("My name is Jordan Handy. Feel free to explore my website, and view some of the work that I've done.");
-aboutParagraph.appendChild(textNode);
 var element = document.querySelector("h2");
-element.append(aboutParagraph);
+element.insertAdjacentHTML('afterend',"<p>My name is Jordan Handy. Feel free to explore my website, and view some of the work that I've done.</p>");
